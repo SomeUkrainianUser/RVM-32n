0f40ae596636588c4a1d49b62463434f1d545c96
SomeUkrainianUser <tymoshenkomaxx@gmail.com>
2025-09-20 14:24:42 +0300

    Synced dev with master

--- Assembler/preprocessor.py
+++ Assembler/preprocessor.py
@@ -11,6 +11,7 @@ def directives(code: str) -> list[str]:
 	while i < len(code):
 		isDefine = code[i].startswith(f'{dir_start_sym}DEFINE')
 		isLabel = code[i].startswith(f'{dir_start_sym}LABEL')
+		isInclude = code[i].startswith(f'{dir_start_sym}INCLUDE')
 
 		if isDefine:
 			parts = code[i].split(' ', 2)
@@ -28,6 +29,26 @@ def directives(code: str) -> list[str]:
 		
 			code.pop(i)
 			i -= 1
+		
+		elif isInclude:
+			parts = code[i].split(' ', 1)
+			if len(parts) == 2:
+				filename = parts[1]
+				try:
+					with open(filename, 'r') as f:
+						included_code = f.read()
+					included_code = directives(included_code)
+					code.pop(i)
+					for line in reversed(included_code):
+						code.insert(i, line)
+					i -= 1
+				except FileNotFoundError:
+					print(f'Error: Included file "{filename}" not found.')
+					code.pop(i)
+					i -= 1
+			else:
+				code.pop(i)
+				i -= 1
 
 		i += 1
 
@@ -36,6 +57,16 @@ def directives(code: str) -> list[str]:
 	for label, value in labels.items():
 		code = code.replace(label, value)
 
+	return code
+
+def macros(code: str) -> str:
+	code = re.sub(r'(LDI|ADD|SUB|MUL|DIV|NEG|SHL|SHR|AND|OR|NAND|NOR|NOT|XOR|XNOR) (\w+), \[([0X]?\w+)\]', r'LDI RAM_PTR, \3\n\1 \2, RAM', code)
+	code = re.sub(r'(LDI|ADD|SUB|MUL|DIV|NEG|SHL|SHR|AND|OR|NAND|NOR|NOT|XOR|XNOR) \[([0X]?\w+)\], ([0X]?\w+)', r'LDI RAM_PTR, \2\n\1 RAM, \3', code)
+	code = re.sub(r'STACK_INIT\n', r'LDI SP, 0\n', code)
+	code = re.sub(r'PUSH (\w+)', r'SUB SP, 1\nLDI RAM_PTR, SP\nLDI RAM, \1', code)
+	code = re.sub(r'POP (\w+)', r'LDI RAM_PTR, SP\nLDI \1, RAM\nADD SP, 1', code)
+	code = re.sub(r'\nPOP\n', r'\nADD SP, 1\n', code)
+	
 	return code
 
 def preprocessor(code: str) -> str:
@@ -54,8 +85,7 @@ def preprocessor(code: str) -> str:
 		i += 1
 
 	code = directives('\n'.join(code))
-	code = re.sub(r'(LDI|ADD|SUB|MUL|DIV|NEG|SHL|SHR|AND|OR|NAND|NOR|NOT|XOR|XNOR) (\w+), \[([0X]?\w+)\]', r'LDI RAM_PTR, \3\n\1 \2, RAM', code)
-	code = re.sub(r'(LDI|ADD|SUB|MUL|DIV|NEG|SHL|SHR|AND|OR|NAND|NOR|NOT|XOR|XNOR) \[([0X]?\w+)\], ([0X]?\w+)', r'LDI RAM_PTR, \2\n\1 RAM, \3', code)
+	code = macros(code)
 	code = code.split('\n')
 	
 	return code
